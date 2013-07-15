366e8a84b8b7f98701f375b4b15c7afc6c532de6
= <=>
2013-07-15 13:35:28 +0800

    new add

---
+++ core/src/main/java/com/hnong/crawler/core/util/AppHolder.java
@@ -0,0 +1,41 @@
+package com.dajie.crawler.core.util;
+
+/**
+ * User: chris
+ * Date: 13-7-13
+ */
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+/**
+ * User: huangzhong
+ * Date: 2011-11-30
+ * Time: 22:17:20
+ */
+public class AppHolder
+{
+    private static Injector injector = Guice.createInjector();
+    private static final Object lock = new Object();
+    private static AppHolder instance;
+
+    private AppHolder(){}
+
+    public Injector getInjector()
+    {
+        return injector;
+    }
+
+    public static AppHolder getInstance()
+    {
+        if (instance == null) {
+            synchronized(lock) {
+                if(instance == null){
+                    instance = new AppHolder();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
